57714c5734c18728f52f58377ec86e04f22dc690
Zack Scholl <zack.scholl@gmail.com>
2021-02-09 15:33:50 -0800

    add glyphs

---
+++ lib/glyphs.lua
@@ -0,0 +1,28 @@
+local Glyphs={}
+
+local f=io.open(_path.code.."kolor/lib/glyphs.json","rb")
+local content=f:read("*all")
+f:close()
+
+local glyphs=json.decode(content)
+
+
+function Glyphs.pixels(name)
+  pixels={}
+  for i=1,#name do
+    letter=name:sub(i,i)
+    for _,glyph in ipairs(glyphs) do
+      if glyph.glyph==letter then
+        for _,position in ipairs(glyph.positions) do
+          table.insert(pixels,{position.x+(i-1)*4+1,position.y+1,15})
+        end
+        break
+      end
+    end
+  end
+
+
+  return pixels
+end
+
+return Glyphs
